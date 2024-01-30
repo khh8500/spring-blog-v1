22a63a278f5aa70083760eb3b30d6af986070a14
khh <khh8500@naver.com>
2024-01-30 11:56:13 +0900

    회원가입완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,7 +1,8 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,26 @@
+package shop.mtcoding.blog.user;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity // 리플렉션
+@Table(name = "user_tb")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(length = 60, nullable = false) // null일 수 없다
+    private String password;
+    private String email;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
