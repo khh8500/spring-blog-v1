d217c1dfc85f4b7ef3f4bda446f16df90e842b48
khh <khh8500@naver.com>
2024-02-19 19:39:55 +0900

    댓글 삭제하기

--- src/main/java/shop/mtcoding/blog/reply/ReplyRepository.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyRepository.java
@@ -12,6 +12,27 @@ public class ReplyRepository {
     private final EntityManager em;
 
     @Transactional
+    public void deleteById(int id) {
+        String q = "delete from reply_tb where id = ?";
+        Query query = em.createNativeQuery(q);
+        query.setParameter(1, id);
+
+        query.executeUpdate();
+    }
+
+    public Reply findById(int id) {
+        String q = "select * from reply_tb where id = ?";
+        Query query = em.createNativeQuery(q, Reply.class);
+        query.setParameter(1, id);
+
+        try {
+            return (Reply) query.getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Transactional
     public void save(ReplyRequest.WriteDTO requestDTO, int userId) {
         Query query = em.createNativeQuery("insert into reply_tb(comment, board_id, user_id, created_at) values(?,?,?, now())");
         query.setParameter(1, requestDTO.getComment());
